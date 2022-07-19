83a234080d5eaad73c195af3a65207728917f892
Debanjan Mitra <61077228+debanjanmitra2010@users.noreply.github.com>
2022-07-19 11:28:48 -0400

    Create main.tf

---
+++ Terraform/01-Basics/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# plan - execute 
+resource "aws_s3_bucket" "my_s3_bucket" {
+    bucket = "my-s3-bucket-dm1994"
+ #   versioning {
+ #       enabled = true
+ #   }
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.my_s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+    name = "my_iam_user_abc"
+}
